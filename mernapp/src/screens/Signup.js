2460e51cd40e78b0fe2095e101b6e45b05dc7f65
bhavyae-pokhrel <bhavyaeprasadpokhrel@gmail.com>
2024-04-13 20:56:20 +0530

    food

--- mernapp/src/screens/Signup.js
+++ mernapp/src/screens/Signup.js
@@ -9,7 +9,7 @@ export default function Signup() {
     e.preventDefault();
     console.log(JSON.stringify({ name: credentials.name, email: credentials.email, password: credentials.password, location: credentials.geolocation }))
     //const response = await fetch("http://localhost:5000/api/creatuser", {
-      const response = await fetch("https://gofood-31co.onrender.com/api/loginuser", {
+      const response = await fetch("https://gofood-31co.onrender.com/api/loginuser", { 
       method: "POST",
       headers: {
         'Content-Type': 'application/json'
