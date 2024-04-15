67a9bd8cb1b5f38b7550257b12e6770d176bdd84
bhavyae-pokhrel <bhavyaeprasadpokhrel@gmail.com>
2024-04-15 12:51:41 +0530

    FOOD

--- mernapp/src/screens/Home.js
+++ mernapp/src/screens/Home.js
@@ -9,8 +9,8 @@ export default function Home() {
  let [search, setSearch] = useState('')
 
   const loadData = async () => {
-    let response = await fetch("http://localhost:5000/api/foodData", {
-    // let response = await fetch("https://go-food-r7mx.onrender.com/api/foodData", {
+    //let response = await fetch("http://localhost:5000/api/foodData", {
+     let response = await fetch("https://go-food-24rg.onrender.com/api/foodData", {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
